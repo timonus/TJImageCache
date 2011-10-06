62f97ea2aab3255af8bdce62d75a807d5490bb25
tjohnsen <tjohnsen@ENQP0450NVDNN214M.(none)>
2011-10-06 15:05:46 -0700

    Added TJImageCacheConnection, a self-managing NSURLConnection subclass with hooks into TJImageCache. Changed some erroneous instance methods to class methods. Started NSURLConnection delegate methods in TJImageCache. Added internet fetching logic to the main "get image" method. Added comments where locking should occur, need to pick a good method for doing this locking.

--- TJImageCache.m
+++ TJImageCache.m
@@ -4,6 +4,70 @@
 #import "TJImageCache.h"
 #import <CommonCrypto/CommonDigest.h>
 
+#pragma mark -
+#pragma mark TJURLConnection
+
+@interface TJImageCacheConnection : NSURLConnection
+
+@property (nonatomic, retain) NSMutableData *data;
+@property (retain) NSMutableArray *delegates;
+@property (nonatomic, retain) NSString *url;
+
++ (TJImageCacheConnection *)connectionWithURL:(NSString *)url delegate:(id)delegate;
+
+@end
+
+@protocol TJURLConnectionDelegate <NSObject>
+
+- (void)connectionDidFinishLoading:(TJImageCacheConnection *)connection;
+- (void)connectionDidFail:(TJImageCacheConnection *)connection;
+
+@end
+
+@implementation TJImageCacheConnection : NSURLConnection
+
+@synthesize data;
+@synthesize delegates;
+@synthesize url;
+
+- (void)dealloc {
+	[data release];
+	[delegates release];
+	[url release];
+	
+	[super dealloc];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)theData {
+	[self.data appendData:theData];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	self.data = [NSMutableData data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	[TJImageCache connectionDidFinishLoading:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	[TJImageCache connection:self didFailWithError:error];
+}
+
++ (TJImageCacheConnection *)connectionWithURL:(NSString *)url delegate:(id)delegate {
+	TJImageCacheConnection *connection = [[TJImageCacheConnection alloc] initWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]] delegate:connection];
+	
+	connection.delegates = [[NSMutableArray alloc] initWithObjects:delegate, nil];
+	connection.url = [[url copy] autorelease];
+	
+	return [connection autorelease];
+}
+
+@end
+
+#pragma mark -
+#pragma mark TJImageCache
+
 @interface TJImageCache ()
 
 + (NSString *)_pathForURL:(NSString *)url;
@@ -35,9 +99,10 @@
 	
 	// Load from memory
 	
-	__block UIImage *image = [[TJImageCache _cache] objectForKey:[TJImageCache _hash:url]];
+	NSString *hash = [TJImageCache _hash:url];
+	__block UIImage *image = [[TJImageCache _cache] objectForKey:hash];
 	
-	// Load from disk...
+	// Load from disk
 	
 	if (!image && depth != TJImageCacheDepthMemory) {
 		
@@ -46,7 +111,7 @@
 			
 			if (image) {
 				// tell delegate about success
-				[[TJImageCache _cache] setObject:image forKey:[TJImageCache _hash:url]];
+				[[TJImageCache _cache] setObject:image forKey:hash];
 				
 				if ([delegate respondsToSelector:@selector(didGetImage:atURL:)]) {
 					[delegate didGetImage:image atURL:url];
@@ -59,6 +124,18 @@
 					dispatch_async(dispatch_get_main_queue(), ^{
 						
 						// Load from the interwebs using NSURLConnection delegate
+						
+						// LOCK
+						
+						if ([[TJImageCache _requests] objectForKey:hash]) {
+							if (delegate) {
+								[[[[TJImageCache _requests] objectForKey:hash] objectForKey:@"delegates"] addObject:delegate];
+							}
+						} else {
+							[[TJImageCache _requests] setObject:[TJImageCacheConnection connectionWithURL:url delegate:delegate] forKey:hash];
+						}
+						
+						// UNLOCK
 						
 					});
 				} else {
@@ -93,22 +170,34 @@
 #pragma mark -
 #pragma mark Cache Manipulation
 
-- (void)removeImageAtURL:(NSString *)url {
++ (void)removeImageAtURL:(NSString *)url {
 	[[TJImageCache _cache] removeObjectForKey:[TJImageCache _hash:url]];
 	
 	[[NSFileManager defaultManager] removeItemAtPath:[TJImageCache _pathForURL:url] error:nil];
 }
 
-- (void)dumpDiskCache {
++ (void)dumpDiskCache {
 	[[NSFileManager defaultManager] removeItemAtPath:[TJImageCache _pathForURL:nil] error:nil];
 }
 
-- (void)dumpMemoryCache {
++ (void)dumpMemoryCache {
 	[[TJImageCache _cache] removeAllObjects];
 }
 
 #pragma mark -
 #pragma mark NSURLConnectionDelegate
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	// LOCK
+	
+	// UNLOCK
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	// LOCK
+	
+	// UNLOCK	
+}
 
 #pragma mark -
 #pragma mark Private
