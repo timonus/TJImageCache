c0f874e379d5927523fc69139398a531ab18a88d
Tim Johnsen <timonus@gmail.com>
2012-08-13 20:59:38 -0700

    Suppress warning

--- TJImageCache.m
+++ TJImageCache.m
@@ -70,7 +70,7 @@
 + (NSString *)hash:(NSString *)string {
 	const char* str = [string UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(str, strlen(str), result);
+    CC_MD5(str, (CC_LONG)strlen(str), result);
 	
     NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
     for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
